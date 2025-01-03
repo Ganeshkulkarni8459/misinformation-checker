e0063a38b5b3be92e40910e53b4fa1fb297593bf
ganeshkulkarni <ganukulkarni8459@gmail.com>
2025-01-03 14:48:00 +0530

    add git ignore

--- run.py
+++ run.py
@@ -5,3 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))  # Default to 8000 if no PORT is set
     app.run(debug=True, host='0.0.0.0', port=port)
+
